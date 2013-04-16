9cf9c9503f234f4fbe01a946c1ac900ffab7cebb
Cecilia Roes <cecilia.roes@gmail.com>
2013-04-16 15:43:21 +0200

    added a class handling the sentiment analysis, currently creates and trains a classifier

---
+++ sentiment_analysis.py
@@ -0,0 +1,24 @@
+import nltk
+from nltk.classify import NaiveBayesClassifier
+
+class SentimentAnalyser:
+
+  def __init__(self, tweets):
+    self.features = self.get_all_features(tweets)
+    training_set = nltk.classify.util.apply_features(
+        self.tweet_features, tweets)
+    self.classifier = NaiveBayesClassifier.train(training_set)
+
+  def get_all_features(self, training_set):
+    wordlist = []
+    for (words, sentiment) in training_set:
+      wordlist.extend(words)
+    wordlist = nltk.FreqDist(wordlist)
+    return wordlist.keys()
+
+  def tweet_features(self, tweet):
+    words = set(tweet)
+    features = {}
+    for feature in self.features:
+      features['contains(%s)' % feature] = (feature in words)
+    return features
